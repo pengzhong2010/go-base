c1a21a0a93be3f5c65f6ae7bf66bcc44f7c5c291
pz <pengzhong2010@gmail.com>
2020-11-13 10:48:27 +0800

    md http

--- plugin/httpclient.go
+++ plugin/httpclient.go
@@ -8,7 +8,7 @@ import (
 )
 
 // 基础方法，这里多用于访问webapi，配合上json转换。此方法可以运行但是不算完善。
-func httpDo(method string, url string, msg string) (string, error) {
+func httpDo(method string, url string, msg string) (int, string, error) {
 	// log.Println("----", url, "----")
 	client := &http.Client{}
 	body := bytes.NewBuffer([]byte(msg))
@@ -17,7 +17,7 @@ func httpDo(method string, url string, msg string) (string, error) {
 		body)
 	if err != nil {
 		log.Println(err)
-		return "", err
+		return 0, "", err
 	}
 
 	req.Header.Set("Content-Type", "application/json;charset=utf-8")
@@ -25,33 +25,41 @@ func httpDo(method string, url string, msg string) (string, error) {
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Println(err)
-		return "", err
+		return 0, "", err
 	}
 
 	defer resp.Body.Close()
 
+	result_code := resp.StatusCode
+
 	result_body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Println(err)
-		return "", err
+		return 0, "", err
 	}
 
-	return string(result_body), nil
+	return result_code, string(result_body), nil
 
 }
 
 // post方式
-func HttpDoPost(url string, msg string) {
-	httpDo("POST", url, msg)
+func HttpDoPost(url string, msg string) (int, string, error) {
+	res_code, res_str, err := httpDo("POST", url, msg)
+	if err != nil {
+		log.Println(err)
+		return 0, "", err
+	}
+
+	return res_code, res_str, nil
 }
 
 // get方式
-func HttpDoGet(url string, msg string) (string, error) {
-	res_str, err := httpDo("GET", url, msg)
+func HttpDoGet(url string, msg string) (int, string, error) {
+	res_code, res_str, err := httpDo("GET", url, msg)
 	if err != nil {
 		log.Println(err)
-		return "", err
+		return 0, "", err
 	}
 
-	return res_str, nil
+	return res_code, res_str, nil
 }
